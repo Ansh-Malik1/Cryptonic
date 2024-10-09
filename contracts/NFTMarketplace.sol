6213ef1a3261d999f4113af969127da6f16487da
Ansh Malik <anshmalik718@gmail.com>
2024-10-09 20:57:25 +0530

    added hardhat and smart contract configuration

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,40 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTMarketplace is ER721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    uint256 listingPrice = 0.025 ether;
+    address payable owner;
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    struct MarketItem{
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold; 
+    }
+
+    event MarketItemCreated(
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    constructor(){
+        owner = payable(message.sender);
+        console.log("%s deployed to:", address(this));
+    }
+}
