680fc398448bad168ed3e2ace0f0f31d90886b2c
Ansh Malik <anshmalik718@gmail.com>
2024-10-10 13:07:43 +0530

    Updated NFTMarketplace.sol contract, hardhat.config.js, and package dependencies also added code for smart contracts using hardhat and solidity langauge

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
+const fs = require('fs');
+// const { network } = require('hardhat');
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const privateKey = fs.readFileSync('.secret').toString().trim()
 module.exports = {
+  networks:{
+    hardhat:{
+      chainId:1337,
+    }, 
+  },
   solidity: "0.8.27",
 };
